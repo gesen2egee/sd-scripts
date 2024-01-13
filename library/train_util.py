1a3f30bb052d23f23a61808790197a16d8257e06
gesen2egee <79357052+gesen2egee@users.noreply.github.com>
2024-01-13 09:09:59 +0800

    REX

--- library/train_util.py
+++ library/train_util.py
@@ -19,6 +19,8 @@ from typing import (
     Sequence,
     Tuple,
     Union,
+    Iterable,
+    Callable,
 )
 from accelerate import Accelerator, InitProcessGroupKwargs, DistributedDataParallelKwargs, PartialState
 import glob
@@ -2875,7 +2877,7 @@ def add_optimizer_arguments(parser: argparse.ArgumentParser):
         "--lr_scheduler",
         type=str,
         default="constant",
-        help="scheduler to use for learning rate / 学習率のスケジューラ: linear, cosine, cosine_with_restarts, polynomial, constant (default), constant_with_warmup, adafactor",
+        help="scheduler to use for learning rate / 学習率のスケジューラ: linear, cosine, cosine_with_restarts, polynomial, constant (default), constant_with_warmup, adafactor, rex",
     )
     parser.add_argument(
         "--lr_warmup_steps",
@@ -4046,6 +4048,30 @@ def get_optimizer(args, trainable_params):
 
     return optimizer_name, optimizer_args, optimizer
 
+def lr_lambda_warmup(warmup_steps: int, lr_lambda: Callable[[int], float]):
+    def warmup(current_step: int):
+        if current_step < warmup_steps:
+            return float(current_step) / float(warmup_steps)
+        else:
+            return lr_lambda(current_step - warmup_steps)
+    return warmup
+
+def lr_lambda_rex(
+        scheduler_steps: int,
+):
+    def lr_lambda(current_step: int):
+        # https://arxiv.org/abs/2107.04197
+        max_lr = 1
+        min_lr = 0.001
+        d = 0.9
+
+        if current_step < scheduler_steps:
+            progress = (current_step / scheduler_steps)
+            div = (1 - d) + (d * (1 - progress))
+            return min_lr + (max_lr - min_lr) * ((1 - progress) / div)
+        else:
+            return min_lr
+    return lr_lambda
 
 # Modified version of get_scheduler() function from diffusers.optimizer.get_scheduler
 # Add some checking and features to the original function.
@@ -4095,6 +4121,16 @@ def get_scheduler_fix(args, optimizer: Optimizer, num_processes: int):
         # logger.info(f"adafactor scheduler init lr {initial_lr}")
         return wrap_check_needless_num_warmup_steps(transformers.optimization.AdafactorSchedule(optimizer, initial_lr))
 
+    if name.upper() == "REX":
+        scheduler_steps = num_training_steps - num_warmup_steps
+        lr_lambda = lr_lambda_rex(scheduler_steps, **lr_scheduler_kwargs)
+        if num_warmup_steps > 0:
+            lr_lambda = lr_lambda_warmup(num_warmup_steps, lr_lambda)
+        return torch.optim.lr_scheduler.LambdaLR(
+                optimizer=optimizer,
+                lr_lambda=lr_lambda,
+        )
+   
     name = SchedulerType(name)
     schedule_func = TYPE_TO_SCHEDULER_FUNCTION[name]
 
