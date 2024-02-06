4c322177dee4e20c3f45dd302fcf3293bf718c53
gesen2egee <79357052+gesen2egee@users.noreply.github.com>
2024-02-06 23:14:02 +0800

    11

--- train_textual_inversion.py
+++ train_textual_inversion.py
@@ -116,9 +116,9 @@ class TextualInversionTrainer:
         noise_pred = unet(noisy_latents, timesteps, text_conds).sample
         return noise_pred
 
-    def sample_images(self, accelerator, args, epoch, global_step, device, vae, tokenizer, text_encoder, unet, prompt_replacements):
+    def sample_images(self, accelerator, args, epoch, global_step, device, vae, tokenizer, text_encoder, unet, prompt_replacement):
         train_util.sample_images(
-            accelerator, args, epoch, global_step, device, vae, tokenizer, text_encoder, unet, prompt_replacements
+            accelerator, args, epoch, global_step, device, vae, tokenizer, text_encoder, unet, prompt_replacement
         )
 
     def save_weights(self, file, updated_embs, save_dtype, metadata):
@@ -316,7 +316,6 @@ class TextualInversionTrainer:
         collator = train_util.collator_class(current_epoch, current_step, ds_for_collator)
 
         # make captions: tokenstring tokenstring1 tokenstring2 ...tokenstringn という文字列に書き換える超乱暴な実装
-        prompt_replacements = []
         if use_template:
             accelerator.print(f"use template for training captions. is object: {args.use_object_template}")
             templates = imagenet_templates_small if args.use_object_template else imagenet_style_templates_small
@@ -329,14 +328,16 @@ class TextualInversionTrainer:
             # サンプル生成用
             if args.num_vectors_per_token > 1:
                 prompt_replacement = (args.token_string, replace_to)
-                prompt_replacements.append(prompt_replacement)
+            else:
+                prompt_replacement = None
         else:
             # サンプル生成用
             if args.num_vectors_per_token > 1:
                 replace_to = " ".join(token_strings)
                 train_dataset_group.add_replacement(args.token_string, replace_to)
                 prompt_replacement = (args.token_string, replace_to)
-                prompt_replacements.append(prompt_replacement)
+            else:
+                prompt_replacement = None
 
         if args.debug_dataset:
             train_util.debug_dataset(train_dataset_group, show_input_ids=True)
@@ -662,7 +663,7 @@ class TextualInversionTrainer:
                         tokenizer_or_list,
                         text_encoder_or_list,
                         unet,
-                        prompt_replacements,
+                        prompt_replacement,
                     )
 
                     # 指定ステップごとにモデルを保存
@@ -745,7 +746,7 @@ class TextualInversionTrainer:
                 tokenizer_or_list,
                 text_encoder_or_list,
                 unet,
-                prompt_replacements,
+                prompt_replacement,
             )
 
             # end of epoch
