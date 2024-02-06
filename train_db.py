7e15837083ef4155573055fa2e78af86e8d7ed5d
gesen2egee <79357052+gesen2egee@users.noreply.github.com>
2024-02-06 22:16:32 +0800

    Revert "Merge branch 'pr/589' into newsd"
    This reverts commit af79f729acf46e39a93cb5cb8aec751f5795e4f7, reversing
    changes made to 3ab2225c33bcbe2bf6524ef38bc65c7f7c887e76.

--- train_db.py
+++ train_db.py
@@ -35,7 +35,6 @@ from library.custom_train_functions import (
     apply_noise_offset,
     scale_v_prediction_loss_like_noise_prediction,
     apply_debiased_estimation,
-    get_latent_masks
 )
 # perlin_noise,
 from library.train_util import (
@@ -341,11 +340,6 @@ def train(args):
                     target = noise_scheduler.get_velocity(latents, noise, timesteps)
                 else:
                     target = noise
-
-                if args.masked_loss and batch['masks'] is not None:
-                    mask = get_latent_masks(batch['masks'], noise_pred.shape, noise_pred.device)
-                    noise_pred = noise_pred * mask
-                    target = target * mask
 
                 loss = torch.nn.functional.mse_loss(noise_pred.float(), target.float(), reduction="none")
                 loss = loss.mean([1, 2, 3])
